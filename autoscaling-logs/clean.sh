c2f9f638905c5eebae49efc4c63d770332820cca
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-02 18:44:42 +0800

    Debug

---
+++ autoscaling-logs/clean.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+set -x
+
+server_exec() {
+    ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
+}
+
+MASTER_NODE=$1
+
+# just workers on params
+shift
+
+SCRIPT_PATH="~/control-plane-scripts/autoscaling-logs"
+
+tmux kill-session -t "exp"
+
+server_exec $MASTER_NODE "$SCRIPT_PATH/terminate-master.sh"
+for worker in "$@"; do
+    server_exec $worker "$SCRIPT_PATH/terminate-worker.sh"
+done
+
+
