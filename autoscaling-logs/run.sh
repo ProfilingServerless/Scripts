44a6d8d5e6f5a4462f2e3c326785757608dedb2e
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-02 18:43:58 +0800

    Debug

--- autoscaling-logs/run.sh
+++ autoscaling-logs/run.sh
@@ -8,6 +8,8 @@ set -x
 # so we start recording and 295th for 105s to ensure we coverd all proccess. (5 second setup time and 10 seconds hold time)
 
 
+./clean.sh mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
+
 tmux new-session -d -s "exp" "cd ~/loader && go run cmd/loader.go --config=cmd/config.json"
 # waiting for load generator to start
 sleep 5
