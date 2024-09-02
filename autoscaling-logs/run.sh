6732de9ff21684f7fe77e5124f8538dee834d678
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-02 18:11:56 +0800

    Debug

--- autoscaling-logs/run.sh
+++ autoscaling-logs/run.sh
@@ -11,6 +11,6 @@ set -x
 tmux new-session -d -s "exp" "cd ~/loader && go run cmd/loader.go --config=cmd/config.json"
 # waiting for load generator to start
 sleep 5
-./record.sh 295 105 mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
+./record.sh 250 150 mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
 
 
