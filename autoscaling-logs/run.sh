6016828e5854934642ad676ca4e1f2065efa02a1
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-02 21:00:39 +0800

    Debug

--- autoscaling-logs/run.sh
+++ autoscaling-logs/run.sh
@@ -7,12 +7,11 @@ set -x
 # Then the truoghput will change at 300th second and logs are recorded for 90 seconds.
 # so we start recording and 295th for 105s to ensure we coverd all proccess. (5 second setup time and 10 seconds hold time)
 
-
 ./clean.sh mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
 
 tmux new-session -d -s "exp" "cd ~/loader && go run cmd/loader.go --config=cmd/config.json"
+
 # waiting for load generator to start
 sleep 5
-./record.sh 250 150 mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
 
-
+./record.sh 0 30 mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
