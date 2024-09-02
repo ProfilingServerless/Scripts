a4fa2c1cfdbefba3fea0920417e3e044b35e671d
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-02 16:31:47 +0800

    Debug

--- autoscaling-logs/record.sh
+++ autoscaling-logs/record.sh
@@ -8,7 +8,16 @@ mkdir -p ~/outs && rm ~/outs/*
 
 WAIT_TIME=$1
 DURATION=$2
+SCRIPT_PATH="~/control-plane-scripts/autoscaling-logs"
 shift 2
+
+common_init() {
+    server_exec $1 "rm -rf ~/control-plane-scripts && cd ~ && git clone https://github.com/ProfilingServerless/control-plane-scripts.git"
+} 
+
+for node in "$@"; do
+    common_init $node
+done
 
 MASTER_NODE=$1
 # to have workers in left args
@@ -17,24 +26,24 @@ shift
 sleep $WAIT_TIME
 echo "Start recording"
 
-server_exec $MASTER_NODE ./record-master.sh 
+server_exec $MASTER_NODE "$SCRIPT_PATH/record-master.sh" 
 
 for worker in "$@"; do
-    server_exec $worker ./record-worker.sh
+    server_exec $worker "$SCRIPT_PATH/record-worker.sh"
 done
 
 echo "Waiting for $DURATION seconds"
 sleep $DURATION
 
-server_exec $MASTER_NODE ./terminate-master.sh
+server_exec $MASTER_NODE "$SCRIPT_PATH/terminate-master.sh"
 server_exec $MASTER_NODE "tar czf ~/outs.tar.gz --directory=~/outs ~"
-scp $MASTER_NODE:/home/users/mghgm/outs.tar.gz ~/outs/master.tar.gz 
+scp $MASTER_NODE:/users/mghgm/outs.tar.gz ~/outs/master.tar.gz 
 
 i=1
 for worker in "$@"; do
-    server_exec $worker ./terminate-worker.sh
+    server_exec $worker "$SCRIPT_PATH/terminate-worker.sh"
     server_exec $worker "tar czf ~/outs.tar.gz --directory=~/outs ~"
-    scp $MASTER_NODE:/home/users/mghgm/outs.tar.gz "~/outs/worker-$i.tar.gz"
+    scp $MASTER_NODE:/users/mghgm/outs.tar.gz "~/outs/worker-$i.tar.gz"
     ((i+=1))
 done
 echo "Terminated all sessions"
