98a25db70a3b952c65aea879b3dd9c8543e65d06
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-03 17:43:29 +0800

    Debug

--- autoscaling-logs/run.sh
+++ autoscaling-logs/run.sh
@@ -7,11 +7,12 @@ set -x
 # Then the truoghput will change at 300th second and logs are recorded for 90 seconds.
 # so we start recording and 295th for 105s to ensure we coverd all proccess. (5 second setup time and 10 seconds hold time)
 
-./clean.sh mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
+./clean.sh mghgm@pc40.cloudlab.umass.edu mghgm@pc27.cloudlab.umass.edu mghgm@pc31.cloudlab.umass.edu mghgm@pc30.cloudlab.umass.edu mghgm@pc38.cloudlab.umass.edu mghgm@pc26.cloudlab.umass.edu
 
 tmux new-session -d -s "exp" "cd ~/loader && go run cmd/loader.go --config=cmd/config.json"
 
 # waiting for load generator to start
 sleep 5
 
-./record.sh 0 30 mghgm@hp167.utah.cloudlab.us mghgm@hp164.utah.cloudlab.us mghgm@hp196.utah.cloudlab.us mghgm@hp197.utah.cloudlab.us mghgm@hp166.utah.cloudlab.us
+./record.sh 250 150 mghgm@pc40.cloudlab.umass.edu mghgm@pc27.cloudlab.umass.edu mghgm@pc31.cloudlab.umass.edu mghgm@pc30.cloudlab.umass.edu mghgm@pc38.cloudlab.umass.edu mghgm@pc26.cloudlab.umass.edu
+
