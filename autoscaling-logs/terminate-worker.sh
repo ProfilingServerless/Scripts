7fd6ee26a5c814f86c7f04cd367b3f7e28550e01
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-04 18:41:35 +0800

    Debug

--- autoscaling-logs/terminate-worker.sh
+++ autoscaling-logs/terminate-worker.sh
@@ -7,6 +7,8 @@ sessions=(
 
 echo "Terminating all log collection tmux sessions..."
 for session in "${!sessions[@]}"; do
+    tmux send-keys -t "$session" C-c
+    sleep 1
     tmux kill-session -t "$session"
     echo "Terminated tmux session '$session'"
 done
