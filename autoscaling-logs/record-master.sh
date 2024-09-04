a06cd65c070d9f7b7c9cf0fd728af958b9e2358f
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-04 18:59:41 +0800

    Debug

--- autoscaling-logs/record-master.sh
+++ autoscaling-logs/record-master.sh
@@ -20,7 +20,7 @@ sessions=(
     ["bind"]="kubectl -n kube-system logs --since=30s -f ${pods[bind]} | grep 'Pod Scheduled Successfully' > ~/outs/bind 2>&1"
     ["create"]="kubectl -n kube-system logs --since=30s -f ${pods[create]} | grep 'Controller created pod' > ~/outs/create 2>&1"
     ["dec"]="kubectl -n knative-serving logs --since=30s -f ${pods[dec]} | grep -E 'For=.* PodCount=.*' > ~/outs/dec 2>&1"
-    ["patch"]="kubectl -n knative-serving logs --since=30s -f ${pods[patch]} | grep -E 'Successfully scaled to' > ~/outs/patch 2>&1"
+    ["patch"]="kubectl -n knative-serving logs --since=30s -f ${pods[patch]} | grep --line-buffered -E 'Successfully scaled to' > ~/outs/patch 2>&1"
     ["patchtest"]="kubectl -n knative-serving logs --since=30s -f ${pods[patch]} | grep -E 'Successfully scaled to'"
     ["ticks"]="kubectl -n knative-serving logs --since=30s -f ${pods[ticks]} | grep -E 'For=.*Ticked' > ~/outs/ticks 2>&1"
 )
--- autoscaling-logs/terminate-master.sh
+++ autoscaling-logs/terminate-master.sh
@@ -13,7 +13,7 @@ echo "Terminating all log collection tmux sessions..."
 for session in "${!sessions[@]}"; do
     tmux send-keys -t "$session" C-c
     sleep 1
-    # tmux kill-session -t "$session"
+    tmux kill-session -t "$session"
     echo "Terminated tmux session '$session'"
 done
 echo "All log collection sessions have been terminated."
