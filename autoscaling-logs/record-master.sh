edef8e70e3ff1358ca5df768ef6a204ccb1b2852
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-04 14:12:26 +0800

    Debug

--- autoscaling-logs/record-master.sh
+++ autoscaling-logs/record-master.sh
@@ -19,9 +19,9 @@ declare -A sessions
 sessions=(
     ["bind"]="kubectl -n kube-system logs --since=30s -f ${pods[bind]} | grep 'Pod Scheduled Successfully' > ~/outs/bind"
     ["create"]="kubectl -n kube-system logs --since=30s -f ${pods[create]} | grep 'Controller created pod' > ~/outs/create"
-    ["dec"]="kubectl -n knative-serving logs --since=30s -f ${pods[dec]} | grep -E 'For=.* PodCount=.*' | jq '.' > ~/outs/dec"
-    ["patch"]="kubectl -n knative-serving logs --since=30s -f ${pods[patch]} | grep -E 'Successfully scaled to' | jq '.' > ~/outs/patch"
-    ["ticks"]="kubectl -n knative-serving logs --since=30s -f ${pods[ticks]} | grep -E 'For=.*Ticked' | jq '.' > ~/outs/ticks"
+    ["dec"]="kubectl -n knative-serving logs --since=30s -f ${pods[dec]} | grep -E 'For=.* PodCount=.*' > ~/outs/dec"
+    ["patch"]="kubectl -n knative-serving logs --since=30s -f ${pods[patch]} | grep -E 'Successfully scaled to' > ~/outs/patch"
+    ["ticks"]="kubectl -n knative-serving logs --since=30s -f ${pods[ticks]} | grep -E 'For=.*Ticked' > ~/outs/ticks"
 )
 
 for session in "${!sessions[@]}"; do
