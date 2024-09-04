ae553fa11b2bf78ecb3a882c5d701b1cbec052c6
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-09-04 14:25:33 +0800

    Debug

--- autoscaling-logs/run.sh
+++ autoscaling-logs/run.sh
@@ -4,8 +4,8 @@ set -x
 
 # The experiment is for 480 seconds
 # No warmup, starts with steady 20 RPS load. 
-# Then the truoghput will change at 300th second and logs are recorded for 90 seconds.
-# so we start recording and 295th for 105s to ensure we coverd all proccess. (5 second setup time and 10 seconds hold time)
+# Then the truoghput will change at 240th second and logs are recorded for 90 seconds.
+# so we start recording and 240 for 120s to ensure we coverd all proccess. (5 second setup time and 10 seconds hold time)
 
 ./clean.sh mghgm@pc40.cloudlab.umass.edu mghgm@pc31.cloudlab.umass.edu mghgm@pc30.cloudlab.umass.edu mghgm@pc38.cloudlab.umass.edu mghgm@pc26.cloudlab.umass.edu
 
@@ -14,5 +14,5 @@ tmux new-session -d -s "exp" "cd ~/loader && go run cmd/loader.go --config=cmd/c
 # waiting for load generator to start
 sleep 5
 
-./record.sh 250 150 mghgm@pc40.cloudlab.umass.edu mghgm@pc31.cloudlab.umass.edu mghgm@pc30.cloudlab.umass.edu mghgm@pc38.cloudlab.umass.edu mghgm@pc26.cloudlab.umass.edu
+./record.sh 240 150 mghgm@pc40.cloudlab.umass.edu mghgm@pc31.cloudlab.umass.edu mghgm@pc30.cloudlab.umass.edu mghgm@pc38.cloudlab.umass.edu mghgm@pc26.cloudlab.umass.edu
 
