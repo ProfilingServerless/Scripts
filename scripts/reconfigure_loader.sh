8a528ee90ea9fa7896e0d9c12b3cd5f85a2e66e9
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-12 18:07:40 +0800

    Debug

--- scripts/reconfigure_loader.sh
+++ scripts/reconfigure_loader.sh
@@ -5,7 +5,7 @@
 function i_install_go {
     cd /tmp && wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz 
-    if [[ ":$PATH:" != *":$GO_BIN_DIR:"* ]]; then
+    if [[ ":$PATH:" != *":/usr/local/go/bin:"* ]]; then
         echo "export PATH=$PATH:/usr/local/go/bin" | tee -a $HOME/.bashrc
     fi
     source $HOME/.bashrc
