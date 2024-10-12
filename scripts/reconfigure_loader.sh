4fbc386a507f2cfb41067c316158fab588cf35b7
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-12 18:01:26 +0800

    Add reconfigure_loader.sh

---
+++ scripts/reconfigure_loader.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+
+# The script is written in a way to keep idempotency
+
+function i_install_go {
+    cd /tmp && wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz 
+    echo "export PATH=$PATH:/usr/local/go/bin" | tee -a $HOME/.bashrc
+    source $HOME/.bashrc
+}
+
+i_install_go
+
