268bbda1f6a8145359441513b8f3aa11606d3444
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-12 18:24:04 +0800

    Refactor

--- scripts/reconfigure_master.sh
+++ scripts/reconfigure_master.sh
@@ -2,13 +2,16 @@
 
 # The script is written in a way to keep idempotency
 
-kubectl -n knative-serving patch deployment activator -p '{"spec": {"template": {"spec": {"containers": [{"name": "activator", "image": "lkondras/activator-ecd51ca5034883acbe737fde417a3d86:rr-policy"}]}}}}'
+function i_set_activator_image {
+    kubectl -n knative-serving patch deployment activator -p '{"spec": {"template": {"spec": {"containers": [{"name": "activator", "image": "lkondras/activator-ecd51ca5034883acbe737fde417a3d86:rr-policy"}]}}}}'
 
-if [ $? -eq 0 ]; then
-    echo "Changed activator image to round-robin policy"
-else
-    echo "Failed to change activator image"
-    exit 1
-fi
+    if [ $? -eq 0 ]; then
+        echo "Changed activator image to round-robin policy"
+    else
+        echo "Failed to change activator image"
+        exit 1
+    fi
+}
 
 
+i_set_activator_image
