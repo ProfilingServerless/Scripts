d2d0853f7b21cfd79ff13bc84e60e739583a5e70
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-18 00:30:10 +0330

    Add worker reocrds commands

--- record/record_worker.sh
+++ record/record_worker.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
 
-sudo journalctl -f -u kubelet | grep --line-buffered -E "Processing pod event|Processing pod event done" > ~/sync_pods.txt
+mkdir -p outs && rm ~/outs/*
+
+declare -A sessions
+sessions=(
+    ["first_seen"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Receiving a new pod' > ~/outs/first_seen.txt"
+    ["startup_duration"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Observed pod startup duration' > ~/outs/startup_duration.txt"
+)
+
+for session in "${!sessions[@]}"; do
+    tmux kill-session -t "$session"
+    tmux new-session -d -s "$session" "${sessions[$session]}"
+    echo "Started tmux session '$session'"
+done
