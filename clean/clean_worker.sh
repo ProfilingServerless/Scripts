78596d2ec519fc343258a55acb29294f6f4a4bb5
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-23 21:12:04 +0330

    Add container to clean_worker.sh

--- clean/clean_worker.sh
+++ clean/clean_worker.sh
@@ -5,3 +5,6 @@ awk -F' ' '{print $NF","$7}' ~/outs/first_seen.txt | sed 's/pod="\([^"]*\)"/\1/'
 
 # pod_name,e2e_duration,watch_observe_running_time
 awk -F' ' '{ print $14","$16","$35}' ~/outs/startup_duration.txt | sed -E 's/pod="([^"]*)",podStartE2EDuration="([0-9.]+)s",([0-9:]+)/\1,\2,\3/' > startup_duration_clean.csv
+
+# pod_name,start_creation,type
+awk -F' ' '/Name:queue-proxy/ { print $NF","$7",queue" } /Name:user-container/ { print $NF","$7",user" }' ~/outs/container.txt | sed 's/pod="\([^"]*\)"/\1/' > container_clean.csv
