95bfbba2efbb3fe967ede837cecde6e0c26b53e9
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-23 21:45:49 +0330

    Add final pod csv sctipt

---
+++ clean/clean_worker.py
@@ -0,0 +1,58 @@
+import csv
+
+FIRST_SEEN='~/outs/first_seen.txt'
+
+# pod: first_seen,sandbox_start,sandbox_end,queue_start,user_start,observed_running,e2e_duration
+pods = dict()
+
+with open('~/outs/first_seen_clean.txt', mode='r', newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row:
+            pod = row[0]
+            if pod in pods:
+                pods[pod][0] = row[1]
+            else:
+                pods[pod] = [row[1], -1, -1, -1, -1, -1, -1]
+
+with open('~/outs/strtup_duration_clean.txt', mode='r', newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row:
+            pod = row[0]
+            if pod in pods:
+                pods[pod][5], pods[pod][6] = row[1], row[2]
+            else:
+                pods[pod] = [-1, -1, -1, -1, -1, row[1], row[2]]
+
+with open('~/outs/sandbox_clean.txt', mode='r', newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row:
+            pod = row[0]
+            if pod in pods:
+                pods[pod][1] = row[1] if row[2] == 'start' else pods[pod][2] = row[1]
+
+            else:
+                pods[pod] = [-1, row[1], -1, -1, -1, -1, -1] if row[2] == 'start' else pods[pod] = [-1, -1, row[1], -1, -1, -1, -1]]
+
+
+with open('~/outs/container_clean.txt', mode='r', newline="") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row:
+            pod = row[0]
+            if pod in pods:
+                pods[pod][3] = row[1] if row[2] == 'queue' else pods[pod][4] = row[1]
+
+            else:
+                pods[pod] = [-1, -1, -1, row[1], -1, -1, -1] if row[2] == 'queue' else pods[pod] = [-1, -1, -1, -1, row[1], -1, -1]
+
+
+with open('~/outs/pods.txt') as f:
+    writer = csv.writer(f)
+    for k, v in pods.items():
+        row = [k] + v
+        writer.writerow([key, value])
+
+
--- clean/clean_worker.sh
+++ clean/clean_worker.sh
@@ -1,13 +1,13 @@
 #/usr/bin/bash
 
 # pod_name,first_seen
-awk -F' ' '{print $NF","$7}' ~/outs/first_seen.txt | sed 's/pod="\([^"]*\)"/\1/' > first_seen_clean.csv
+awk -F' ' '{print $NF","$7}' ~/outs/first_seen.txt | sed 's/pod="\([^"]*\)"/\1/' > ~/outs/first_seen_clean.csv
 
 # pod_name,e2e_duration,watch_observe_running_time
-awk -F' ' '{ print $14","$16","$35}' ~/outs/startup_duration.txt | sed -E 's/pod="([^"]*)",podStartE2EDuration="([0-9.]+)s",([0-9:]+)/\1,\2,\3/' > startup_duration_clean.csv
+awk -F' ' '{ print $14","$16","$35}' ~/outs/startup_duration.txt | sed -E 's/pod="([^"]*)",podStartE2EDuration="([0-9.]+)s",([0-9:]+)/\1,\2,\3/' > ~/outs/startup_duration_clean.csv
 
 # pod_name,start_creation,type
-awk -F' ' '/Name:queue-proxy/ { print $NF","$7",queue" } /Name:user-container/ { print $NF","$7",user" }' ~/outs/container.txt | sed 's/pod="\([^"]*\)"/\1/' > container_clean.csv
+awk -F' ' '/Name:queue-proxy/ { print $NF","$7",queue" } /Name:user-container/ { print $NF","$7",user" }' ~/outs/container.txt | sed 's/pod="\([^"]*\)"/\1/' > ~/outs/container_clean.csv
 
 # pod_name,timestamp,end/start
-awk -F' ' '/Creating/ { print $NF","$7",start" } /Created/ { print $NF","$7",end" }' ~/outs/sandbox.txt | sed 's/pod="\([^"]*\)"/\1/' > sandbox_clean.csv
+awk -F' ' '/Creating/ { print $NF","$7",start" } /Created/ { print $NF","$7",end" }' ~/outs/sandbox.txt | sed 's/pod="\([^"]*\)"/\1/' > ~/outs/sandbox_clean.csv
