d8afe2b28dd05c16e2a245cd90f18c02af1012ac
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2024-10-31 19:01:52 +0330

    Add remove containers

--- record/record_worker.sh
+++ record/record_worker.sh
@@ -6,7 +6,6 @@ declare -A sessions
 sessions=(
     ["first_seen"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Receiving a new pod' > ~/outs/first_seen.txt"
     ["sandbox"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Creating PodSandbox for pod|Created PodSandbox for pod' > ~/outs/sandbox.txt"
-    ["container"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Creating container in pod' > ~/outs/container.txt"
     ["startup_duration"]="sudo journalctl -f -u kubelet | grep --line-buffered -E 'Observed pod startup duration' > ~/outs/startup_duration.txt"
 )
 
